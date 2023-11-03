481b566e8c02c82340efadd91cb5e00b769596f2
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-03 14:46:51 +0100

    feat: implement a redacted logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,25 @@ which obfuscates specific fields from an input string."""
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        filtered_msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        record.msg = filtered_msg
+        return super().format(record)
 
 
 def filter_datum(
@@ -12,7 +31,17 @@ def filter_datum(
 ) -> str:
     """obfuscate specified fields from input string."""
     pattern = re.compile(
-        r"(?P<field>{})=(?P<value>[^{}]+)".format("|".join(fields), separator))
+        r"(?P<field>{})=(?P<value>[^{}]+)".format("|".join(fields), separator)
+    )
     out = re.sub(
         pattern, lambda m: m.group("field") + "=" + redaction, message)
     return out
+
+
+if __name__ == "__main__":
+    message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;\
+    password=bobby2019;"
+    log_record = logging.LogRecord(
+        "my_logger", logging.INFO, None, None, message, None, None)
+    formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+    print(formatter.format(log_record))
