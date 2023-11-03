2662210e137ccfcbbda475e6d8312ad0cd62b6f9
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-03 16:12:36 +0100

    annotate fn

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -55,7 +55,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the database."""
     uname = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
