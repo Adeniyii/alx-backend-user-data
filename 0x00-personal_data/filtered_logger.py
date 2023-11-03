dcfdab47f4ddb389338c408cdae0af37e143075c
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-03 14:48:25 +0100

    add docs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,6 +20,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum."""
         filtered_msg = filter_datum(
             self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
         record.msg = filtered_msg
