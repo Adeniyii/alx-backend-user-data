8495538f725784cc472a6179bda94b154677c36c
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-03 19:06:51 +0100

    feat: check if encrypted pwd is valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,7 +9,14 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """returns a boolean indicating whether or not the provided password
+    matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
+
 if __name__ == "__main__":
     password = "MyAmazingPassw0rd"
-    print(hash_password(password))
-    print(hash_password(password))
+    hashed_password = hash_password(password)
+    print(hashed_password)
+    print(is_valid(hashed_password, password))
