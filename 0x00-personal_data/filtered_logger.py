b1cea2e3bac37ea5720ded4faebd7545b343b96e
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-03 15:26:31 +0100

    feat: setup custom info logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,8 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
@@ -37,6 +39,17 @@ def filter_datum(
     out = re.sub(
         pattern, lambda m: m.group("field") + "=" + redaction, message)
     return out
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object."""
+    logger = logging.getLogger("user_data")
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.INFO)
+    return logger
 
 
 if __name__ == "__main__":
