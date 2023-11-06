e081b26a7dc8d18f7b29d0f65863328bf8d9ae6c
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-07 00:54:51 +0100

    feat: implement excluded paths method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,11 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """"""
-        return False
+        if path and excluded_paths:
+            if not path.endswith("/"):
+                path += "/"
+            return path in excluded_paths
+        return True
 
     def authorization_header(self, request=None) -> str:
         """"""
