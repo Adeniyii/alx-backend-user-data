2fed01098e8ae3cf842056815729c903d976ebf6
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-07 22:48:41 +0100

    fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic auth module for the app
 """
-from typing import List
+from typing import List, TypeVar
 
 
 class BasicAuth():
@@ -16,3 +16,8 @@ class BasicAuth():
                 path += "/"
             return path not in excluded_paths
         return True
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current User
+        """
+        return None
