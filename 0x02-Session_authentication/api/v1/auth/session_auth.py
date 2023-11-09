d69d854b40009bd023da160658536b9e462e866d
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-09 19:36:29 +0100

    feat: get user from request cookie

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from flask.json import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -25,3 +26,13 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return type(self).user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Get the current user from the request cookie.
+        """
+        if request is None:
+            return None
+        sesh_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(sesh_id)
+
+        return User.get(user_id)
