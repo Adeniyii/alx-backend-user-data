5f893c5f23d52b1f020a2f80dc59e22e8e638673
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-10 17:17:54 +0100

    feat: overload session methos

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,6 +2,7 @@
 """ Session authentication
 """
 from api.v1.auth.session_exp_auth import SessionExpAuth
+import datetime
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -16,3 +17,49 @@ class SessionDBAuth(SessionExpAuth):
 
         UserSession(user_id=user_id, session_id=session_id).save()
         return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """Returns a User ID based on a Session ID
+        """
+        if session_id is None:
+            return None
+
+        from models.user_session import UserSession
+
+        sesh_obj_list = UserSession.search({"session_id": session_id})
+        if len(sesh_obj_list) == 0:
+            return None
+        seshObj = sesh_obj_list[0]
+
+        if self.session_duration <= 0 or self.session_duration is None:
+            return seshObj.get("user_id")
+
+        createdAt = seshObj.get("created_at")
+        if createdAt is None:
+            return None
+
+        exp = datetime.timedelta(seconds=self.session_duration) + createdAt
+        if exp < datetime.datetime.now():
+            return None
+
+        return seshObj.get("user_id")
+
+    def destroy_session(self, request=None):
+        """Delete the user session / log out.
+        """
+        if request is None:
+            return False
+
+        sesh_id = self.session_cookie(request)
+        if not sesh_id:
+            return False
+
+        from models.user_session import UserSession
+
+        sesh_obj_list = UserSession.search({"session_id": sesh_id})
+        if len(sesh_obj_list) == 0:
+            return False
+
+        seshObj = sesh_obj_list[0]
+        seshObj.remove()
+        return True
