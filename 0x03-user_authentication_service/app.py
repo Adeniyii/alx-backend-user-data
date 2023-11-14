1a0afc4240a3f0bdb122d6395b167571c0f24967
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-14 20:43:16 +0100

    feat: implement register user route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Module `app.py` sets up a Flask application.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,6 +16,27 @@ def home() -> str:
       - a simple json message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """ POST /users
+    Form data:
+      - email
+      - password
+    Return:
+      - 200 if created successfully
+      - 400 if can't create the new User
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        Auth.register_user(email=email, password=password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
