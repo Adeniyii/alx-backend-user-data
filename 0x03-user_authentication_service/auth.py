5d6b5cf695ebe4e28edbfaf0e1c2ef0380ffe952
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-14 20:04:35 +0100

    feat: register a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,28 @@
 """Auth module defines authentication utilities.
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user if not exists
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(
+                email, _hash_password(password).decode("utf-8"))
+
+        raise ValueError("User {} already exists".format(email))
 
 
 def _hash_password(password: str) -> bytes:
