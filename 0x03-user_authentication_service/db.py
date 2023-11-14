4d8a9cf4001c11d77c57fd24cc4703fe5af8da2d
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-14 16:40:52 +0100

    feat: find user by kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,6 +2,7 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -42,3 +43,12 @@ class DB:
             new_user = None
 
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find user by kwargs
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise (NoResultFound)
+
+        return user
