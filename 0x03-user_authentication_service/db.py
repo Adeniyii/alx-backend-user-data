82ff390db56317cdca368b3ef6d5a3cf0a4d47ad
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-14 16:56:10 +0100

    feat: update a user by their id

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,3 +48,14 @@ class DB:
             raise (NoResultFound)
 
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user by ID
+        """
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if not hasattr(User, k):
+                raise ValueError
+            setattr(user, k, v)
+
+        self._session.commit()
