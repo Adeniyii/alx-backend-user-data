39aa8b739e8acc0730571b5b4ad1251eba1781cc
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-17 18:30:25 +0100

    feat: get user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,5 +83,21 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """ GET /profile
+    Cookie data:
+      - session_id
+    Return:
+      - 200 {"email": "<user email>"}
+      - 403 if session_id matches no user
+    """
+    sesh_id = request.cookies.get(SESH_ID, "")
+    user = AUTH.get_user_from_session_id(sesh_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
