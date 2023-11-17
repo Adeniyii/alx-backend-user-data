f9ef4e585ed5904a294c93546f157cab78e6776a
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-17 18:47:22 +0100

    feat: impl route to request a new reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -99,5 +99,22 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route("/reset_password", method=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """ POST /reset_password
+    Form data:
+      - email
+    Return:
+      - The reset token payload
+      - 403 if email is invalid
+    """
+    email = request.form.get("email")
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
