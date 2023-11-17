2824e1584558f3d3c16bfac0a64ff4bbcd6bd4f8
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-17 19:17:32 +0100

    feat: impl reset password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -117,5 +117,28 @@ def get_reset_password_token() -> str:
     return jsonify({"email": email, "reset_token": token})
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """ PUT /reset_password
+    Form data:
+      - email
+      - new_password
+      - reset_token
+    Return:
+      - The reset password payload
+      - 403 if reset token is invalid
+    """
+    email = request.form.get("email", "")
+    new_password = request.form.get("new_password", "")
+    reset_token = request.form.get("reset_token", "")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
